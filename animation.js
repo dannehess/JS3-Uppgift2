4ef270745d84d709a949606d77716b4dc759a5ff
Daniel Hessling <dannehess@gmail.com>
2019-08-21 14:55:39 +0200

    Added header-section

--- animation.js
+++ animation.js
@@ -3,10 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
   anime.timeline({
   })
   .add({
+    targets: '#appleicon-white',
+    opacity: [0, 1],
+    translateX: [-200, 0],
+    duration: 5000})
+  .add({
       targets: '.heading',
       opacity: [0, 1],
       translateX: [-800, 0],
-      duration: 3000})
+      duration: 3000,
+      offset: 0
+    })
   .add({
       targets: '.heading-two',
       translateY: [-800, 0],
@@ -17,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
       translateX: [-520],
       duration: 4000,
       offset: 2400,
-      scale: [0.5, 1]
+      scale: [0.5, 1],
+      offset: 0
   })
 })
